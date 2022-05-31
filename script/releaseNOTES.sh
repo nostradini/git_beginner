95a0de194e98025d4eb3319e372d9bef5b4f45cf
nostradini <60344282+nostradini@users.noreply.github.com>
2022-05-31 00:58:05 -0700

    128

--- script/releaseNOTES.sh
+++ script/releaseNOTES.sh
@@ -6,12 +6,12 @@ echo "user= $user , repo= $repo"
 echo "cred = $ENV_TOKEN"
 
 echo "$(git log --after="2022-05-31T01:16:29Z" --format=oneline)"
-arrCom=
 
 arrCom=()
 while IFS= read -r line; do
     arrCom+=( "$line" )
 done < <( "$(git log --after="2022-05-31T01:16:29Z" --format=oneline)" )
+echo "arrCom = " $arrCom[0]
 
 tag=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
