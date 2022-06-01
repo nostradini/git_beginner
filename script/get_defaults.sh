dbb20fbafe73afa6fcd8c20b9a1aa57d083f6328
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-01 01:17:55 -0700

    115

--- script/get_defaults.sh
+++ script/get_defaults.sh
@@ -15,5 +15,5 @@ cn=$((cn+1))
 done < <( git log --after="$targetD" --format=oneline )
 
 echo " arrCOUNT= ${#arrCom[@]} , CN= $cn"
-echo "COUNT:$cn" >> $github_ENV
+echo "COUNT:$cn" >> $GITHUB_ENV
 echo "data == \n" $data
