e40c8db09787b88ec5b91da7ab9d76f62535bd5e
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-02 19:58:09 -0700

    b11 let there be light

--- script/releaseNOTES.sh
+++ script/releaseNOTES.sh
@@ -22,7 +22,7 @@ echo "tag = $tag , prevtag = $prevtag , new = $envVer"
 
 data="# v$envVer  $(date "+%F-%H-%M-%S")"
 echo "data initial= $data"
-data="$data \n ## $envGM \n ### * Major Changes \n $envMj \n ### * Minor Changes \n $envMn \n ### * Patches \n $envPt"
+data="$data \n ## $envGM \n ### * Major Changes \n $envMj ### * Minor Changes \n $envMn ### * Patches \n $envPt"
 
 prep_data()
 {
