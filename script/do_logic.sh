dec43534ccd4031df31477dafdfabd0f8b13e8fa
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-03 00:11:33 -0700

    test on #patch

--- script/do_logic.sh
+++ script/do_logic.sh
@@ -28,21 +28,21 @@ then
     then
     echo "Found major in commit"
     bMajor=true
-    colMajor="$colMajor - ##### ${line:0:7} - ${line:41:50} \n "
+    colMajor="$colMajor    - ##### ${line:0:7} - ${line:41:50} \n "
     elif [[ "${lowerstr}" == *"#minor"* ]]
     then
     echo "Found minor in commit"
     bMinor=true
-    colMinor="$colMinor  - ##### ${line:0:7} - ${line:41:50} \n "
+    colMinor="$colMinor    - ##### ${line:0:7} - ${line:41:50} \n "
     elif [[ "${lowerstr}" == *"#patch"* ]]
     then
     echo "Found patch in commit"
     bPatch=true
-    colPatch="$colPatch  - ##### ${line:0:7} - ${line:41:50} \n "
+    colPatch="$colPatch    - ##### ${line:0:7} - ${line:41:50} \n "
     else
     # echo "Default condition"
     bDefault=true
-    colDefault="$colDefault  - #### ${line:0:7} - ${line:41:50} \n "
+    colDefault="$colDefault    - #### ${line:0:7} - ${line:41:50} \n "
     fi
 fi
 done < <( git log --after="$targetD" --format=oneline )
