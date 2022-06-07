da6f9127322d0176359ab7754baf45629fc9715d
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-06 20:26:53 -0700

    cl114

--- script/changeLOG.sh
+++ script/changeLOG.sh
@@ -12,73 +12,71 @@ bDefault=false
 
 echo "User= $USER , REPO= $REPO"
 
-content=$(curl \
+targetD=$(curl \
 -H "Accept: application/vnd.github.v3+json" \
-https://api.github.com/repos/nostradini/git_beginner/releases/latest | jq .name,.body)
-
-# targetD=$(curl \
-# -H "Accept: application/vnd.github.v3+json" \
-# https://api.github.com/repos/$user/$repo/releases/latest | jq .created_at)
+https://api.github.com/repos/$user/$repo/releases/latest | jq .created_at)
 
 # targetD="2022-06-01T01:00:00Z"
-# echo "Date = $targetD"
+echo "Date = $targetD"
+
+
+arrCom=()
+while IFS= read -r line; do
+lowerstr=$(echo ${line:41:50}|tr '[:upper:]' '[:lower:]')
+echo "transformed to lower = $lowerstr"
+if [[ "${lowerstr}" != "[job]"* ]]
+then
+    if [[ "${lowerstr}" == *"#major"* ]]
+    then
+    echo "Found major in commit"
+    bMajor=true
+    colMajor="$colMajor <h4><li> ${line:0:7} - ${line:41:50} <li/><h4/><br>"
+    elif [[ "${lowerstr}" == *"#minor"* ]]
+    then
+    echo "Found minor in commit"
+    bMinor=true
+    colMinor="$colMinor <h4><li> ${line:0:7} - ${line:41:50} <li/><h4/><br>"
+    elif [[ "${lowerstr}" == *"#patch"* ]]
+    then
+    echo "Found patch in commit"
+    bPatch=true
+    colPatch="$colPatch <h4><li> ${line:0:7} - ${line:41:50} <li/><h4/><br>"
+    else
+    # echo "Default condition"
+    bDefault=true
+    colDefault="$colDefault <h4><li> ${line:0:7} - ${line:41:50} <li/><h4/><br>"
+    fi
+fi
+done < <( git log --after="$targetD" --format=oneline )
+
+if [[ $bMajor == true ]]
+then
+    gitmojiko=":boom: Breaking Changes"
+    ver_major=$((ver_major+1))
+    ver_minor=0
+    ver_patch=0
+elif [[ $bMinor == true ]]
+then
+    gitmojiko=":sparkles: New Features"
+    ver_minor=$((ver_minor+1))
+    ver_patch=0
+elif [[ $bPatch == true ]]
+then
+    gitmojiko=":bug: Bug Fixes"
+    ver_patch=$((ver_patch+1))
+else
+    gitmojiko="UNRELEASED"
+fi
+echo "gitmojiko= $gitmojiko"
+echo "colMajor= $colMajor"
+echo "colMinor= $colMinor"
+echo "colPatch= $colPatch"
+
+echo "NEW= major=$ver_major,minor=$ver_minor,patch=$ver_patch"
+
+content="<h1> CHANGELOG <h1/> <br> <h2> $gitmojiko <h2/> <br><ul> $colMajor $colMinor $colPatch <ul/>"
 
 echo "content= $content"
-# arrCom=()
-# while IFS= read -r line; do
-# # arrCom+=( "$line" )
-# ## handlle char case
-# lowerstr=$(echo ${line:41:50}|tr '[:upper:]' '[:lower:]')
-# echo "transformed to lower = $lowerstr"
-# if [[ "${lowerstr}" != "[job]"* ]]
-# then
-#     if [[ "${lowerstr}" == *"#major"* ]]
-#     then
-#     echo "Found major in commit"
-#     bMajor=true
-#     colMajor="$colMajor \n    - ##### ${line:0:7} - ${line:41:50} \n"
-#     elif [[ "${lowerstr}" == *"#minor"* ]]
-#     then
-#     echo "Found minor in commit"
-#     bMinor=true
-#     colMinor="$colMinor \n    - ##### ${line:0:7} - ${line:41:50} \n"
-#     elif [[ "${lowerstr}" == *"#patch"* ]]
-#     then
-#     echo "Found patch in commit"
-#     bPatch=true
-#     colPatch="$colPatch \n    - ##### ${line:0:7} - ${line:41:50} \n"
-#     else
-#     # echo "Default condition"
-#     bDefault=true
-#     colDefault="$colDefault \n    - #### ${line:0:7} - ${line:41:50} \n"
-#     fi
-# fi
-# done < <( git log --after="$targetD" --format=oneline )
-# echo "$bMajor - $bMinor - $bPatch"
-# if [[ $bMajor == true ]]
-# then
-#     gitmojiko=":boom: Breaking Changes"
-#     ver_major=$((ver_major+1))
-#     ver_minor=0
-#     ver_patch=0
-# elif [[ $bMinor == true ]]
-# then
-#     gitmojiko=":sparkles: New Features"
-#     ver_minor=$((ver_minor+1))
-#     ver_patch=0
-# elif [[ $bPatch == true ]]
-# then
-#     gitmojiko=":bug: Bug Fixes"
-#     ver_patch=$((ver_patch+1))
-# else
-#     gitmojiko="UNRELEASED"
-# fi
-# echo "gitmojiko= $gitmojiko"
-# echo "colMajor= $colMajor"
-# echo "colMinor= $colMinor"
-# echo "colPatch= $colPatch"
-
-# echo "NEW= major=$ver_major,minor=$ver_minor,patch=$ver_patch"
 
 # echo "trLC= $trLC"
 # LenlastComm=${#trLC}
@@ -89,7 +87,7 @@ echo "content= $content"
 # else
 #     trimLC=${trLC}
 # fi
-# echo "trimmed = " $trimLC
+# echo "trimmed = " $trimLC 
 # cat /dev/null > ./VERSION
 # echo -n "Version $ver_major.$ver_minor.$ver_patch - $trimLC" > ./VERSION
 
