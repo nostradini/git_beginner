0c691d96acc468a3758940642ab57f68ea73b410
nostradini <60344282+nostradini@users.noreply.github.com>
2022-06-07 18:49:14 -0700

    cl test more

--- script/changeLOG.sh
+++ script/changeLOG.sh
@@ -29,21 +29,21 @@ then
     then
     # echo "Found major in commit"
     bMajor=true
-    colMajor="$colMajor <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li><br>"
+    colMajor="$colMajor <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li>"
     elif [[ "${lowerstr}" == *"#minor"* ]]
     then
     # echo "Found minor in commit"
     bMinor=true
-    colMinor="$colMinor <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li><br>"
+    colMinor="$colMinor <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li>"
     elif [[ "${lowerstr}" == *"#patch"* ]]
     then
     # echo "Found patch in commit"
     bPatch=true
-    colPatch="$colPatch <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li><br>"
+    colPatch="$colPatch <li><a href='https://github.com/$user/$repo/commit/${line:0:7}'> ${line:0:7} </a> - ${line:41:50} </li>"
     else
     # echo "Default condition"
     bDefault=true
-    colDefault="$colDefault <li> ${line:0:7} - ${line:41:50} </li><br>"
+    colDefault="$colDefault <li> ${line:0:7} - ${line:41:50} </li>"
     fi
 fi
 done < <( git log --after="$targetD" --format=oneline )
@@ -76,7 +76,7 @@ echo "newVer= $newVER"
 
 if [[ ${#colMajor} != 0 ]]
 then
-    MjTitle="<ul><br><li><h4>Major Changes</h4></li><br>"
+    MjTitle="<ul><<li><h4>Major Changes</h4></li>"
     if [[ ${#colMinor} != 0 ]] || [[ ${#colPatch} != 0 ]]
     then
     colMajor="<ul><h6>$colMajor</h6></ul>"
@@ -89,9 +89,9 @@ if [[ ${#colMinor} != 0 ]]
 then
     if [[ ${#colMajor} != 0  ]]
     then
-    MnTitle="<li><h4>Minor Changes</h4></li><br>"
+    MnTitle="<li><h4>Minor Changes</h4></li>"
     else
-    MnTitle="<ul><br><li><h4>Minor Changes</h4></li><br>"
+    MnTitle="<ul><br><li><h4>Minor Changes</h4></li>"
     fi
     if [[ ${#colPatch} != 0 ]]
     then
@@ -104,17 +104,20 @@ if [[ ${#colPatch} != 0 ]]
 then
     if [[ ${#colMinor} != 0 ]] || [[ ${#colMajor} != 0 ]]
     then
-    PtTitle="<li><h4>Patches</h4></li><br>"
+    PtTitle="<li><h4>Patches</h4></li>"
     colPatch="<ul><h5>$colPatch</h5></ul></ul>"
     else
-    PtTitle="<ul><br><li><h4>Patches</h4></li><br>"
+    PtTitle="<ul><br><li><h4>Patches</h4></li>"
     colPatch="<ul><h5>$colPatch</h5></ul></ul>"
     fi
 
 fi
 
-echo "MjTitle=$MjTitle,MnTitle=$MnTitle,PtTitle=$PtTitle"
-content="<h1>CHANGELOG</h1><br><h2>$newVER - $(date '+%F%H%M%S')</h2><br><h3>$gitmojiko</h3> $MjTitle $colMajor $MnTitle $colMinor $PtTitle $colPatch"
+d1=$(date +"%m-%d-%y")
+d2=$(date +"%r"_)
+varDate="d1-d2"
+# echo "MjTitle=$MjTitle,MnTitle=$MnTitle,PtTitle=$PtTitle"
+content="<h1>CHANGELOG</h1><h2>$newVER - $varDate</h2><h3>$gitmojiko</h3> $MjTitle $colMajor $MnTitle $colMinor $PtTitle $colPatch"
 
 echo "content= $content"
 
